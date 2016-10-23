b1c2bd5dc7033972ea71973375be1ac55b454a70
Joseph Sutton <josesutt@cisco.com>
2016-10-23 02:41:09 -0700

    (update) code (update) compose to v2 (add) events into code base

---
+++ build.sh
@@ -0,0 +1,133 @@
+#!/bin/bash
+
+export PWD=$(pwd)
+
+# if Mac OSX then set env to docker machine so docker commands work
+if [ "$(uname)" == "Darwin" ]; then
+	eval "$(docker-machine env default)"
+fi
+
+echo "--------------------------------------------"
+echo "Taiga Setup Wizard"
+echo "--------------------------------------------"
+
+error () {
+	echo "$1" >&2
+	kill -INT $$
+}
+
+yesNoPrompt () {
+	read -p "$2 [Yes/No] (default: $1)?" -n 1 var
+	echo "" >&2
+	case "$choice" in 
+  		y|Y ) echo "Yes";;
+  		n|N ) echo "No";;
+  		* ) echo "$1";;
+	esac
+}
+
+prompt () {
+	if [ -z "$3" ]; then
+		read -p "$2 (default: $1)?" var
+
+		if [ -z "$var" ]; then
+			var=$1
+		fi
+	elif [ "$3" = "--required" ]; then
+
+		if [ -z "$1" ]; then
+			read -p "$2 (ex. $4)?" var
+
+			if [ -z "$var" ]; then
+				error "$2 is require"
+			fi
+		else
+			read -p "$2 (enter to use: $1)?" var
+
+			if [ -z "$var" ]; then
+				var=$1
+			fi
+		fi
+	fi
+
+	echo $var
+	return 0
+}
+
+CLEANUP_CONTAINERS=$(yesNoPrompt "No" "Do you want to stop and remove Taiga services")
+if [ "$CLEANUP_CONTAINERS" = "Yes" ]; then
+	# # Stop all containers
+	# docker stop $(docker ps -a -q)
+	# # Delete all containers
+	# docker rm $(docker ps -a -q)
+
+	# Stop service containers
+	docker-compose stop
+	# Delete service containers
+	docker-compose rm
+fi
+
+CLEANUP_IMAGES=$(yesNoPrompt "No" "Do you want to remove Taiga images before we start")
+if [ "$CLEANUP_IMAGES" = "Yes" ]; then
+	# # Delete all images
+	# docker rmi $(docker images -q)
+
+	# Delete taiga images
+	docker rmi taiga-events
+	docker rmi taiga
+fi
+
+
+export TAIGA_HOST=$(prompt "$TAIGA_HOST" "Frontend Hostname" --required "taiga.mysite.com" )
+export TAIGA_PORT=$(prompt "${TAIGA_PORT-8000}" "Frontend Port")
+
+export EMAIL_HOST=$(prompt "$(docker-machine ip)" "Email Hostname")
+export EMAIL_PORT=$(prompt "25" "Email Port")
+export EMAIL_HOST_USER=$(prompt "" "Email Login Username")
+export EMAIL_HOST_PASSWORD=$(prompt "" "Email Login Password")
+
+EMAIL_USETLS=$(yesNoPrompt "No" "Email use TLS")
+if [ "$EMAIL_USETLS" = "Yes" ]; then
+	export EMAIL_USE_TLS="True"
+else
+	export EMAIL_USE_TLS="False"
+fi
+
+GITHUB=$(yesNoPrompt "No" "Github Integration")
+if [ "$GITHUB" = "Yes" ]; then
+	export GITHUB_URL=$(prompt "https://github.com/" "Github URL")
+	export GITHUB_API_URL=$(prompt "https://api.github.com/" "Github API URL")
+	export GITHUB_API_CLIENT_ID=$(prompt "$GITHUB_API_CLIENT_ID" "Github API Client ID" --required "yourClientId")
+	export GITHUB_API_CLIENT_SECRET=$(prompt "$GITHUB_API_CLIENT_SECRET" "Github API Client Secret" --required "yourClientSecret")
+else
+	export GITHUB_URL=""
+	export GITHUB_API_URL=""
+	export GITHUB_API_CLIENT_ID=""
+	export GITHUB_API_CLIENT_SECRET=""
+fi
+
+echo "--------------------------------------------"
+echo "Hostname: $TAIGA_HOST"
+echo "Port: $TAIGA_PORT"
+
+echo "Email use TLS: $EMAIL_USE_TLS"
+echo "Email Hostname: $EMAIL_HOST"
+echo "Email Port: $EMAIL_PORT"
+echo "Email Login User: $EMAIL_HOST_USER"
+echo "Email Login Password: $EMAIL_HOST_PASSWORD"
+
+echo "Github URL: $GITHUB_URL"
+echo "Github API URL: $GITHUB_API_URL"
+echo "Github API Client ID: $GITHUB_API_CLIENT_ID"
+echo "Github API Client Secret: $GITHUB_API_CLIENT_SECRET"
+echo "--------------------------------------------"
+
+echo "--------------------------------------------"
+echo "Build Services..."
+echo "--------------------------------------------"
+docker-compose build
+
+echo "--------------------------------------------"
+echo "Creating Containers..."
+echo "--------------------------------------------"
+docker-compose create
--- docker-settings.py
+++ docker-settings.py
@@ -4,7 +4,7 @@ from .common import *
 
 DATABASES = {
     'default': {
-        'ENGINE': 'transaction_hooks.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('TAIGA_DB_NAME'),
         'HOST': os.getenv('POSTGRES_PORT_5432_TCP_ADDR') or os.getenv('TAIGA_DB_HOST'),
         'USER': os.getenv('TAIGA_DB_USER'),
@@ -38,3 +38,106 @@ if os.getenv('RABBIT_PORT') is not None and os.getenv('REDIS_PORT') is not None:
 
     EVENTS_PUSH_BACKEND = "taiga.events.backends.rabbitmq.EventsPushBackend"
     EVENTS_PUSH_BACKEND_OPTIONS = {"url": "amqp://guest:guest@rabbit:5672"}
+
+
+#########################################
+## GENERIC
+#########################################
+#MEDIA_ROOT = '/home/taiga/media'
+#STATIC_ROOT = '/home/taiga/static'
+
+#########################################
+## MAIL SYSTEM SETTINGS
+#########################################
+
+#DEFAULT_FROM_EMAIL = "john@doe.com"
+#CHANGE_NOTIFICATIONS_MIN_INTERVAL = 300 #seconds
+
+EMAIL_USE_TLS = False
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+
+# EMAIL SETTINGS EXAMPLE
+#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+#EMAIL_USE_TLS = False
+#EMAIL_HOST = 'localhost'
+#EMAIL_PORT = 25
+#EMAIL_HOST_USER = 'user'
+#EMAIL_HOST_PASSWORD = 'password'
+
+# GMAIL SETTINGS EXAMPLE
+#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+#EMAIL_USE_TLS = True
+#EMAIL_HOST = 'smtp.gmail.com'
+#EMAIL_PORT = 587
+#EMAIL_HOST_USER = 'youremail@gmail.com'
+#EMAIL_HOST_PASSWORD = 'yourpassword'
+
+
+#########################################
+## REGISTRATION
+#########################################
+
+#PUBLIC_REGISTER_ENABLED = True
+
+# LIMIT ALLOWED DOMAINS FOR REGISTER AND INVITE
+# None or [] values in USER_EMAIL_ALLOWED_DOMAINS means allow any domain
+#USER_EMAIL_ALLOWED_DOMAINS = None
+
+# PUCLIC OR PRIVATE NUMBER OF PROJECT PER USER
+#MAX_PRIVATE_PROJECTS_PER_USER = None # None == no limit
+#MAX_PUBLIC_PROJECTS_PER_USER = None # None == no limit
+#MAX_MEMBERSHIPS_PRIVATE_PROJECTS = None # None == no limit
+#MAX_MEMBERSHIPS_PUBLIC_PROJECTS = None # None == no limit
+
+# GITHUB SETTINGS
+if os.getenv('GITHUB_URL'):
+    GITHUB_URL = os.getenv('GITHUB_URL')
+    GITHUB_API_URL = os.getenv('GITHUB_API_URL')
+    GITHUB_API_CLIENT_ID = os.getenv('GITHUB_API_CLIENT_ID')
+    GITHUB_API_CLIENT_SECRET = os.getenv('GITHUB_API_CLIENT_SECRET')
+
+#GITHUB_URL = "https://github.com/"
+#GITHUB_API_URL = "https://api.github.com/"
+#GITHUB_API_CLIENT_ID = "yourgithubclientid"
+#GITHUB_API_CLIENT_SECRET = "yourgithubclientsecret"
+
+
+#########################################
+## SITEMAP
+#########################################
+
+# If is True /front/sitemap.xml show a valid sitemap of taiga-front client
+#FRONT_SITEMAP_ENABLED = False
+#FRONT_SITEMAP_CACHE_TIMEOUT = 24*60*60  # In second
+
+
+#########################################
+## FEEDBACK
+#########################################
+
+# Note: See config in taiga-front too
+#FEEDBACK_ENABLED = True
+#FEEDBACK_EMAIL = "support@taiga.io"
+
+
+#########################################
+## STATS
+#########################################
+
+#STATS_ENABLED = False
+#FRONT_SITEMAP_CACHE_TIMEOUT = 60*60  # In second
+
+
+#########################################
+## CELERY
+#########################################
+
+from .celery import *
+CELERY_ENABLED = True
+
+# To use celery in memory
+#CELERY_ENABLED = True
+#CELERY_ALWAYS_EAGER = True

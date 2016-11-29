56232325b2b4e232cde4ca4a656da98f43204a48
Joseph Sutton <josesutt@cisco.com>
2016-11-29 11:18:54 -0800

    (fix) init to use git@ instead of https so it works behind proxy

--- init.sh
+++ init.sh
@@ -9,7 +9,7 @@ if [ -d "./taiga-back/src" ]; then
 	echo "Already cloned"
 else
 	cd taiga-back
-	git clone https://github.com/taigaio/taiga-back.git src
+	git clone git@github.com:taigaio/taiga-back.git src
 	cd ..
 fi
 
@@ -20,7 +20,7 @@ if [ -d "./taiga-front-dist/src" ]; then
 	echo "Already cloned"
 else
 	cd taiga-front-dist
-	git clone https://github.com/taigaio/taiga-front-dist.git src
+	git clone git@github.com:taigaio/taiga-front-dist.git src
 	cd ..
 fi
 
@@ -31,6 +31,6 @@ if [ -d "./taiga-events/src" ]; then
 	echo "Already cloned"
 else
 	cd taiga-events
-	git clone https://github.com/taigaio/taiga-events src
+	git clone git@github.com:taigaio/taiga-events.git src
 	cd ..
 fi
